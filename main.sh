69a5abb232d637e593b034f29d396a6917bbb054
Risingfeanyx <35235934+Risingfeanyx@users.noreply.github.com>
2020-07-12 08:14:45 -0400

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+EDITOR=nano
+PASSWD=/etc/passwd
+RED='\033[0;41;30m'
+STD='\033[0;0;39m'
+mkdir Gentoomen ; cd Gentoomen
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+one(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Algorithms/"; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+ 
+two(){
+		wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Animation/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+three(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Artificial%20Intelligence/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+four(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Computational%20Linguistics/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+five(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Computer%20Architecture/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+
+        pause
+}
+
+six(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Computer%20Graphics/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+seven(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Computer%20History/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+eight(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Cryptography/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+nine(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/DSP-Collection/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+ten(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Data%20Analysis/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+eleven(){
+       wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Data%20Mining/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+twelve(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Data%20Structures/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+thirteen(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Databases/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+fourteen(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Digital%20Design/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+fifteen(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Electronics/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+sixteen(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Extra/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+seventeen(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Game%20Development/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+eighteen(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Information%20Retrieval/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+nineteen(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Information%20Theory/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twenty(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Maths/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentyone(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Misc/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentytwo(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Networking/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentythree(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Operating%20Systems/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentyfour(){
+	wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Programming/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+twentyfive(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Security/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentysix(){
+        owget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Software%20Engineering/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentyseven(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/The%20Actually%20Useful%20Programming%20Library/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentyeight(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/Theory%20Of%20Computation/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+twentynine(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/HumbleBundle/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+thirty(){
+        wget -m -np -c -U "eye01" -R "index.html*" "https://the-eye.eu/public/Books/Gentoomen%20Library/" ; clear ; find the-eye.eu/ -name  "*.pdf"
+        pause
+}
+
+ 
+# function to display menus
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
+	echo "Welcome to the Gentoomen Library"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "1. Algorithms/"
+	echo "2. Animation/"
+	echo "3. Artificial Intelligence/"
+	echo "4. Computational Linguistics/"
+	echo "5. Computer Architecture/"
+	echo "6. Computer Graphics/"
+	echo "7. Computer History/"
+	echo "8. Cryptography/"
+	echo "9. DSP-Collection/"
+	echo "10. Data Analysis/"
+	echo "11. Data Mining/"
+	echo "12. Data Structures/"
+	echo "13. Databases/" 
+	echo "14. Digital Design/"
+	echo "15. Electronics/"
+	echo "16. Extra/"
+	echo "17. Game Development/"
+	echo "18. Information Retrieval/"
+	echo "19. Information Theory/"
+	echo "20. Maths/"
+	echo "21. Misc/"
+	echo "22. Networking/"
+	echo "23. Operating Systems/"
+	echo "24. MProgramming/"
+	echo "25. Security/"
+	echo "26. Software Engineering/"
+	echo "27. The Actually Useful Programming Library/"
+	echo "28. Theory Of Computation/"
+	echo "29. Humble Bundle Collection"
+	echo "30. Download the whole library! +35 GB; so it will be a long while"
+}
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 29] " choice
+	case $choice in
+		1) one ;;
+		2) two ;;
+		3) three ;;
+		4) four ;;
+		5) five ;;
+		6) six ;;
+		7) seven ;;
+		8) eight ;;
+		9) nine ;;
+		10) ten ;;
+		11) eleven ;;
+		12) twelve ;;
+		13) thirteen ;;
+		14) fourteen ;;
+		15) fifteen ;; 
+		16) sixteen ;;
+		17) seventeen ;;
+		18) eighteen ;;
+		19) nineteen ;;
+		20) twenty ;;
+		21) twentyone ;;
+		22) twentytwo ;;
+		23) twentythree ;;
+		24) twentyfour ;;
+		25) twentyfive ;;
+		26) twentysix ;;
+		27) twentyseven ;;
+		28) twentyeight ;;
+		29) twentynine ;;
+		30) thirty ;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+ 
+# ----------------------------------------------
+# Step #3: Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------------------
+#trap '' SIGINT SIGQUIT SIGTSTP
+ 
+# -----------------------------------
+# Step #4: Main logic - infinite loop
+# ------------------------------------
+while true
+do
+ 
+	show_menus
+	read_options
+done
